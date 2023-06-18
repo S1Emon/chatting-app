e2048009ffbe980263db7c3f9363d9113e36e829
S1Emon <shahariaremon.bd71@gmail.com>
2023-06-19 01:57:09 +0600

    settings added

--- src/Pages/Settings/index.js
+++ src/Pages/Settings/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 import "./style.css";
 import { useSelector } from "react-redux";
+import Accountforms from "./Accountforms";
 
 const Settings = () => {
   const user = useSelector((users) => users.loginSlice.login);
   return (
     <div className="account-info">
       <div className="account-info-box">
-        <div className="profile-pictures"></div>
+        <div className="profile-pictures">
+          <img src={user.photoURL} alt="profile pic" loading="lazy" />
+        </div>
+        <div className="account-name">
+          <Accountforms />
+        </div>
       </div>
     </div>
   );
